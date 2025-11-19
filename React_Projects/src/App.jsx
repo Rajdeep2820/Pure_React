e091f1a112db87c3cdef3a5e433b1b276e34edfb
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-11-19 10:13:47 +0530

    Loading all the components together.

--- React_Projects/src/App.jsx
+++ React_Projects/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-// import Accordian from './components/accordian'
+import Accordian from './components/accordian'
 import StarRating from './components/Star-Rating'
+import LoadMore from './components/Load-More'
 function App() {
   return (
     <div className='App'>
-      {/* <Accordian /> */}
+      <Accordian /> 
       <StarRating/>
+      <LoadMore/>
     </div>
   )
 }
